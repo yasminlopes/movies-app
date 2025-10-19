c62ccec3cd7b705aa7e4eb5028e40590ab6a70d7
Yasmin Lopes <yclopes27@gmail.com>
2025-10-19 19:54:05 -0300

    chore: add required polyfills for react-router and other dependencies

---
+++ jest.setup.js
@@ -0,0 +1,20 @@
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+global.fetch = jest.fn();
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
